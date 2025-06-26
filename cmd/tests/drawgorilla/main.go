c50c508d23ef7b1e85e32e8e1b43f41451e66700
Arran Ubels <arran4@gmail.com>
2025-06-27 09:45:29 +1000

    Add standard image drawing package

--- cmd/tests/drawgorilla/main.go
+++ cmd/tests/drawgorilla/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"image"
 	"image/color"
 	"image/png"
 	"os"
 
-	ebdraw "github.com/arran4/gorillas/drawings/ebiten"
-	"github.com/hajimehoshi/ebiten/v2"
+	imgdraw "github.com/arran4/gorillas/drawings/img"
 )
 
 func main() {
 	scale := 2.0
-	img := ebiten.NewImage(int(30*scale), int(30*scale))
-	ebdraw.DrawBASGorilla(img, 15*scale, scale, scale, ebdraw.ArmsDown, color.RGBA{150, 75, 0, 255})
+	img := image.NewRGBA(image.Rect(0, 0, int(30*scale), int(30*scale)))
+	imgdraw.DrawBASGorilla(img, 15*scale, scale, scale, imgdraw.ArmsDown, color.RGBA{150, 75, 0, 255})
 	f, err := os.Create("gorilla.png")
 	if err != nil {
 		panic(err)
