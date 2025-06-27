2d3c2de7fae51819091e85719ba18f18784152e9
Arran Ubels <arran4@gmail.com>
2025-06-27 21:54:40 +1000

    Fix joystick stub fields for non-Linux builds

--- cmd/gorillia-tcell/joystick_stub.go
+++ cmd/gorillia-tcell/joystick_stub.go
@@ -2,7 +2,10 @@
 
 package main
 
-type joystick struct{}
+type joystick struct {
+    axis [2]int16
+    btn  [16]bool
+}
 
 func openJoystick() (*joystick, error) { return nil, nil }
 func (j *joystick) poll()              {}
